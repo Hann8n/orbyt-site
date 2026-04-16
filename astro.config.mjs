fac7d1ae6b8f1a57c246a0f10c373a13f684157c
jack.orbyt.video <did:plc:tjio2pnbsuc6ps77kocywwmc>
2026-04-15 21:56:19 -0400

    chore: update dependencies and enhance configuration for improved performance
    - Add Vite optimization configuration to exclude 'fsevents' for better dependency management.
    - Upgrade dependencies in package.json and package-lock.json for @astrojs/cloudflare and astro to their latest versions.
    - Update devDependency @astrojs/check to the latest version for improved functionality.
    - Refine TypeScript configuration in tsconfig.json for better path mapping.
    - Introduce responsive styles in embed.css and profile.css to enhance media presentation on smaller screens.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,11 @@ import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://getorbyt.com',
+  vite: {
+    optimizeDeps: {
+      exclude: ['fsevents'],
+    },
+  },
   adapter: cloudflare({
     imageService: 'cloudflare',
     platformProxy: {
